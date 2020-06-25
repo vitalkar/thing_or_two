a1d59af4d9f818fdf4addbf7a67d80a494315477
vitalkar <vitalykar1@gmail.com>
2020-06-25 17:14:46 +0300

    removed some sensitive info

--- inc/config.class.php
+++ inc/config.class.php
@@ -4,7 +4,7 @@ class config
     public static $db = array(
         'host' => 'localhost',
         'db_name' => 'thing_or_two',
-        'username' => 'vit_mmd',
-        'password' => 'vit_mmd'
+        'username' => '',
+        'password' => ''
     );
 }
